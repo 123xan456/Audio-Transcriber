b19bd8ae19d2a6a91ea9d4b7cc1a5c52b8865c80
123xan456 <lxiaan27@gmail.com>
2022-11-10 09:54:33 +0800

    delete unnecessary files

--- pyannotetest.py
+++
@@ -1,6 +0,0 @@
-from pyannote.audio import Pipeline
-pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization")
-dz = pipeline(str("audio/Sample 3.wav"))
-
-for turn, _, speaker in dz.itertracks(yield_label=True):
-    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s {speaker}")
--- whispertest.py
+++
@@ -1,6 +0,0 @@
-import whisper
-
-model = whisper.load_model("small")
-
-result = model.transcribe("audio/Sample 3.wav",language = "en")
-print(result["text"])
