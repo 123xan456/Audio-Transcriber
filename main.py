1f0605caa6622adb7eea9fe18c677110bf0c3a8f
123xan456 <lxiaan27@gmail.com>
2022-11-10 09:51:38 +0800

    use NeMo

--- main.py
+++ main.py
@@ -1,80 +1,18 @@
-# @app.route("/result", methods=["POST"])
-# def result():
-#     pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token="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")
-#     model = whisper.load_model("medium")
-#     file = request.files["file"]
-#     filename = secure_filename(file.filename)
-#     file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-
-#     #add 1 second silence to beginning and end of uploaded audio file
-#     audio = AudioSegment.from_wav(str(UPLOAD_FOLDER + "/" + filename))
-#     silent  = AudioSegment.silent(duration=1000)
-#     audio = silent.append(audio)
-#     audio = audio.append(silent)
-#     audio.export(str(UPLOAD_FOLDER + "/" + filename), format="wav")
-
-#     dz = pipeline(str(UPLOAD_FOLDER + "/" + filename))
-
-#     print(dz)
-
-#     text_file = open("diarization.txt", "r+")
-#     text_file.write(str(dz))
-#     stringdz = str(dz).splitlines()
-#     dzlist = []
-#     lines = []
-#     file_to_transcribe = silent
-
-#     for l in stringdz:
-#         # find start and end of each speaker section in milliseconds, and which speaker
-#         start, end = tuple(re.findall("[0-9]+:[0-9]+:[0-9]+\.[0-9]+", string=l))
-#         start = millisec(start)
-#         end = millisec(end)
-#         speaker = re.findall("SPEAKER_[0-9]+", string=l)
-#         dzlist.append([start, end, speaker[0]])
-
-#         # cut audio into speaker sections, append to file to be transcribed with silence in between
-
-#         section = audio[start:end]
-#         file_to_transcribe = file_to_transcribe + section + silent
-
-
-#     file_to_transcribe.export("file_to_transcribe.wav", format="wav")
-#     result = model.transcribe("file_to_transcribe.wav", language="en")
-#     result = result["text"]
-
-#     silent_parts = silence.detect_silence(file_to_transcribe)
-#     print(silent_parts)
-#     # os.remove("file_to_transcribe.wav")
-
-#     # result = []
-
-#     # for i in lines:
-#     #     result.append(str(i[0]) + " : " + str(i[1]))
-#     # print(result)
-
-#     os.remove("uploads/" + filename)
-
-#     df.loc[len(df.index)] = [filename, result]
-#     df.to_excel("results.xlsx")
-#     return render_template("result.html", result=result, filename=filename)
-
 from pyannote.audio import Pipeline
 from pydub import AudioSegment
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-import textrank, math, re, os, whisper, pandas
+import textrank, re, os, whisper, pandas, time, json
+from omegaconf import OmegaConf
+from nemo.collections.asr.parts.utils.decoder_timestamps_utils import ASR_TIMESTAMPS
+from nemo.collections.asr.parts.utils.diarization_utils import ASR_DIAR_OFFLINE
+from pathlib import Path
 
 UPLOAD_FOLDER = "uploads"
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-df = pandas.read_excel("results.xlsx", index_col=0)
-
-
-def millisec(timeStr):
-    spl = timeStr.split(":")
-    millisec = (int)((int(spl[0]) * 3600 + int(spl[1]) * 60 + float(spl[2])) * 1000)
-    return millisec
+df = pandas.read_excel("/home/xiann/pyannote/results.xlsx", index_col=0)
 
 
 @app.route("/")
@@ -84,6 +22,14 @@ def upload():
 
 @app.route("/result", methods=["POST"])
 def result():
+    start = time.time()
+
+    # obtain audio file, and save
+    file = request.files["file"]
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    audio = AudioSegment.from_wav(str(UPLOAD_FOLDER + "/" + filename))
+
     # load diarization and transciption models
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization@2.1",
@@ -91,82 +37,108 @@ def result():
     )
     model = whisper.load_model("medium")
 
-    # obtain audio file, and save
-    file = request.files["file"]
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    # transcribe entire clip. split text every 500 words
+    transcription = model.transcribe(str(UPLOAD_FOLDER + "/" + filename), language="en")
+    transcription_list = transcription["text"].split()
+    split_sections = [
+        " ".join(transcription_list[i : i + 500])
+        for i in range(0, len(transcription_list), 500)
+    ]
 
-    # for summary of entire clip, 100 words every 10 mins
-    start = 0
-    end = 600000
-    full_transcription = ""
+    # summarize each section and join
     full_summary = ""
-    dzlist = []
+    for section in split_sections:
+        summary = textrank.extract_sentences(section)
+        full_summary += " " + summary
+
+    # diarize
+    AUDIO_FILENAME = str(UPLOAD_FOLDER + "/" + filename)
+    CONFIG_NEMO = "config/diar_infer_telephonic.yaml"
+    cfg_nemo = OmegaConf.load(CONFIG_NEMO)
+
+    meta = {
+        "audio_filepath": AUDIO_FILENAME,
+        "offset": 0,
+        "duration": None,
+        "label": "infer",
+        "text": "-",
+        "num_speakers": 2,
+        "rttm_filepath": None,
+        "uem_filepath": None,
+    }
+
+    with open("input_manifest.json", "w") as fp:
+        json.dump(meta, fp)
+        fp.write("\n")
+
+    cfg_nemo.diarizer.manifest_filepath = "input_manifest.json"
+    pretrained_speaker_model = "titanet_large"
+    cfg_nemo.diarizer.out_dir = (
+        "../data"  # Directory to store intermediate files and prediction outputs
+    )
+    cfg_nemo.diarizer.speaker_embeddings.model_path = pretrained_speaker_model
+    cfg_nemo.diarizer.clustering.parameters.oracle_num_speakers = False
+
+    # Using VAD generated from ASR timestamps
+    cfg_nemo.diarizer.asr.model_path = "QuartzNet15x5Base-En"
+    cfg_nemo.diarizer.oracle_vad = False  # ----> Not using oracle VAD
+    cfg_nemo.diarizer.asr.parameters.asr_based_vad = True
+    cfg_nemo.diarizer.asr.parameters.threshold = (
+        100  # ASR based VAD threshold: If 100, all silences under 1 sec are ignored.
+    )
+    cfg_nemo.diarizer.asr.parameters.decoder_delay_in_sec = (
+        0.2  # Decoder delay is compensated for 0.2 sec
+    )
+    asr_ts_decoder = ASR_TIMESTAMPS(**cfg_nemo.diarizer)
+    asr_model = asr_ts_decoder.set_asr_model()
+    word_hyp, word_ts_hyp = asr_ts_decoder.run_ASR(asr_model)
+
+    asr_diar_offline = ASR_DIAR_OFFLINE(**cfg_nemo.diarizer)
+    asr_diar_offline.word_ts_anchor_offset = asr_ts_decoder.word_ts_anchor_offset
+    diar_hyp, diar_score = asr_diar_offline.run_diarization(cfg_nemo, word_ts_hyp)
+
     lines = []
-    print("hi3")
 
-    # find number of 10min sections, pydub uses milliseconds
-    audio = AudioSegment.from_wav(str(UPLOAD_FOLDER + "/" + filename))
-    audio_length = audio.duration_seconds * 1000
-    num_sections = math.ceil(audio_length / 600000)
-    print("hi4")
+    for li in diar_hyp[Path(str(UPLOAD_FOLDER + "/" + filename)).stem]:
+        line_info = li.split()  # split string into [start, end, speaker]
+        sp_start, sp_end, speaker = (
+            int(float(line_info[0]) * 1000),
+            int(float(line_info[1]) * 1000),
+            line_info[2],
+        )
 
-    for i in range(num_sections):
-        section = audio[start:end]
-        print("reach")
-        # add 2 second silence to beginning and end of section
+        # add 2 second silence to beginning and end of section, cut audio by speaker, save, transcribe
+        section = audio[sp_start:sp_end]
         silence = AudioSegment.silent(duration=2000)
         section = silence.append(section, crossfade=0)
         section = section.append(silence, crossfade=0)
         section.export("section.wav", format="wav")
 
-        meeting_in_text = model.transcribe("section.wav", language="en")
-        summary = textrank.extract_sentences(meeting_in_text["text"])
-        print(summary)
-        full_transcription += meeting_in_text["text"]
-        full_summary += summary
-
-        dz = pipeline("section.wav")
-        stringdz = str(dz).splitlines()
-
-        for l in stringdz:
-            # find start and end of each speaker section in milliseconds, and which speaker
-            sp_start, sp_end = tuple(
-                re.findall("[0-9]+:[0-9]+:[0-9]+\.[0-9]+", string=l)
-            )
-            sp_start = millisec(sp_start)
-            sp_end = millisec(sp_end)
-
-            speaker = re.findall("SPEAKER_[0-9]+", string=l)
-            dzlist.append([sp_start, sp_end, speaker[0]])
-
-            # cut audio into speaker sections, save each section, transcribe, then delete
-            section = audio[sp_start:sp_end]
-            section.export("section.wav", format="wav")
-            line = model.transcribe("section.wav", language="en")
-            lines.append([speaker[0], line["text"]])
-            os.remove("section.wav")
-
-        start += 600000
-        end += 600000
+        # transcribe
+        line = model.transcribe("section.wav", language="en")
+        lines.append([speaker[0], line["text"]])
+        os.remove("section.wav")
 
     result = []
-    for line in lines:
-        if line[1] == "":
+    for newline in lines:
+        if newline[1] == "":
             continue
-        result.append(str(line[0]) + " : " + str(line[1]))
+        result.append(str(newline[0]) + " : " + str(newline[1]))
 
     os.remove("uploads/" + filename)
 
     df.loc[len(df.index)] = [filename, result]
     df.to_excel("results.xlsx")
 
+    time_taken = time.time() - start
+
     return render_template(
         "result.html",
         result=result,
         filename=filename,
         full_summary=full_summary,
-        full_transcription=full_transcription,
+        transcription=transcription["text"],
+        time_taken=time_taken,
     )
 
 
